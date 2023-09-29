d02c102e1db7425e7db0144d5264d6eb00e4f8ad
sairamarapu <saikrishna.ramarapu03@gmail.com>
2023-09-29 15:32:22 +0530

    DAY3

--- DAY2/fib.py
+++ DAY2/fib.py
@@ -1,17 +1,18 @@
-def fib(a):
+'''def fib(a):
     if a==1 :
         return 1
     if a==0:
         return 0
     return fib(a-2)+fib(a-1)
 a=int(input())  
-print(fib(a))
-'''n1=0
+print(fib(a))'''
+a=int(input())
+n1=0
 n2=1
 i=0
-while(i<10):
-    print(n1,end=" ")
+while(i<a):
     temp=n2
     n2=n2+n1
     n1=temp
-    i=i+1'''
+    i=i+1
+print(n1)
---
+++ DAY3/p.py
@@ -0,0 +1,10 @@
+#after creating an array find out the smallest missing positive integer
+'''n=int(input())
+li=list(map(int,input().strip().split()))[:n]
+for i in range(len(li)):
+    if i in li:
+        continue
+    else:
+        print(i)
+        break  '''
+
