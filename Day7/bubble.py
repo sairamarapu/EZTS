e5cc67ecae0b83716baf6991f590a80d2cca0b3b
sairamarapu <saikrishna.ramarapu03@gmail.com>
2023-10-04 15:26:35 +0530

    change

--- Day7/bubble.py
+++ Day7/bubble.py
@@ -1,4 +1,15 @@
+
 a=[6,8,9,5,4,3,26,2]
-counter==0
-while(len(a)<counter):
-    
+counter=0
+flag=0
+while(len(a)>counter):
+    j=0
+    for i in range(1,len(a)-counter):
+        if a[j]>a[i]:
+            a[j],a[i]=a[i],a[j]
+            flag=1
+        j=j+1 
+    if(flag==0):
+        break    
+    counter=counter+1
+print(a)
